ee318e335806ae5c14c942289f79a7e4821598a4
Chris Russell-Walker <c.russell.walker@gmail.com>
2012-11-28 15:19:54 -0500

    markup fix

--- application/views/pages/resume.php
+++ application/views/pages/resume.php
@@ -112,3 +112,4 @@
 
 <div class="separator"><img src="<?php print(base_url()) ?>assets/images/dottedlines.gif" id="top-seperator" alt="line" /></div>
 <h4 id="pdf-link"><a href="<?php print(base_url()) ?>assets/resources/resume.pdf" target="_blank">.pdf format</a></h4>
+</div>
