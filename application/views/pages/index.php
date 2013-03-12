9f293fc401633f67a7bdce73e5b8f71f93c1061c
chrisrw <chrisrw@hungryfishmedia.com>
2013-03-11 21:31:39 -0400

    add sublime ignore and change index page copy

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -7,11 +7,13 @@
 						
 						<h4 class="intro">Welcome</h4>
 						<p>
-								I'm a cook, web developer, musician &amp; amateur brewer of beer and kombucha.
+								I'm a front end developer, cook, musician &amp; amateur brewer of beer and kombucha.
 								I live in Cambridge, MA. and when not pursuing these ventures I enjoy cycling, reading, traveling, kickboxing and laughing at anything funny.
 						</p>
 						<p>
-								I've worked for a <a href="http://www.zooluweb.com" title="website design shop">web development and design company located in New York - <strong>Zoolu Web Solutions</strong></a>
+								I work at <a href="http://www.nutraclick.com" target="_blank"><strong>NutraClick</strong></a> as a front end developer tackling things small, large and extraordinary.  
+								The challenges encountered so far have strengthened my skills and solidified my commitment to becoming the most well rounded developer I can be.  
+								I've worked for a web development and design company located in New York - <a href="http://www.zooluweb.com" title="website design shop"><strong>Zoolu Web Solutions</strong></a>
 								as well as a financial consulting firm <a href="http://www.truebridge.com"><strong>Truebridge</strong></a> expanding on a browser based CRM.
 								These jobs complemented my learning while completing my CS degree from BU and playing as much music as I could.
 						</p>
